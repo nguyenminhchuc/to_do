89cea90e7da9b7aaced71a5f41325376c7428e06
chucnm-1901 <nguyen.minh.chuc@sun-asterisk.com>
2019-10-17 20:18:57 +0700

    add task

--- Gemfile
+++ Gemfile
@@ -27,7 +27,8 @@ gem 'webpacker'
 gem 'react-rails'
 gem 'bootstrap', '~> 4.3.1'
 gem 'jquery-rails'
-
+gem 'responders'
+gem 'faker'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  attr_reader :seed
+  def initialize
+    @seed
+  end
+
+  def run
+    generate_tasks
+  end
+
+  def generate_tasks
+    50.times do
+      task = Task.new
+      task.name = Faker::Lorem.word
+      task.details = Faker::Lorem.sentence(100)
+      task.status = [0, 1, 2].sample
+      task.save!
+      puts "Generated task # #{task.id}"
+    end
+  end
+end
+
+seed = Seed.new
+seed.run
