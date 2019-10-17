509c03e2f20eac852c3f3ae9ec6f2508f69c28e1
chucnm-1901 <nguyen.minh.chuc@sun-asterisk.com>
2019-10-17 21:01:28 +0700

    new task

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_125059) do
+ActiveRecord::Schema.define(version: 2019_10_17_134204) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.text "details"
-    t.integer "status"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
